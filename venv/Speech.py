89eb90402668036920b9a04e197555be87ebc3e0
Mindaugas <mindaugasm88@gmail.com>
2019-04-27 16:45:24 +0100

    Initial commit

---
+++ venv/Speech.py
@@ -0,0 +1,55 @@
+import pyaudio
+import wave
+import speech_recognition as sr
+
+def play_audio(filename):
+
+    chunk = 1024
+    wf = wave.open(filename, 'rb')
+    pa = pyaudio.PyAudio()
+
+    stream = pa.open(
+            format=pa.get_format_from_width(wf.getsampwidth()),
+            channels=wf.getnchannels(),
+            rate=wf.getframerate(),
+            output=True
+    )
+
+    data_stream = wf.readframes(chunk)
+
+    while data_stream:
+        stream.write(data_stream)
+        data_stream = wf.readframes(chunk)
+
+    stream.close()
+    pa.terminate()
+
+
+
+r = sr.Recognizer()
+
+def initSpeech():
+
+    print("Listening...")
+
+    play_audio("venv/Audio/definite.wav")
+
+    with sr.Microphone() as source:
+        print("Say Something")
+        audio = r.listen(source)
+
+    play_audio("venv/Audio/slow-spring-board.wav")
+
+    command = ""
+
+    try:
+        command = r.recognize_google(audio)
+
+    except:
+        print("Couldn't understand you")
+
+    print("Your command: ")
+    print(command)
+
+initSpeech()
+
